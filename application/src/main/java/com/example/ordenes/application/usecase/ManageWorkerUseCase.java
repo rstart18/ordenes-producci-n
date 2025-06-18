997bb51e31018612f0fa9db980d36274caccdd79
rstart18 <40774070+rstart18@users.noreply.github.com>
2025-06-17 22:22:26 -0500

    Add worker update endpoint

--- application/src/main/java/com/example/ordenes/application/usecase/ManageWorkerUseCase.java
+++ application/src/main/java/com/example/ordenes/application/usecase/ManageWorkerUseCase.java
@@ -2,6 +2,7 @@ package com.example.ordenes.application.usecase;
 
 import com.example.ordenes.domain.model.Worker;
 import com.example.ordenes.domain.model.CreatedWorker;
+import com.example.ordenes.domain.model.UpdatedWorker;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,8 @@ public interface ManageWorkerUseCase {
     Optional<Worker> get(Long id);
 
     Optional<CreatedWorker> create(String name, String job);
+
+    Optional<UpdatedWorker> update(Long id, String name, String job);
 
     List<Worker> list(int page);
 }
